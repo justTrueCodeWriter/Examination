f6f5a9bd5c82a9485a0920ed2251434a1555524f
justTrueCodeWriter <dmjustcoder8@gmail.com>
2023-12-08 12:53:03 +0300

    upd theory view && upd test view style

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
-import sqlite3
+from src.exam_functions import getDataFromDatabase
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html") 
 
+@app.route('/theory')
+def theory():
+    return render_template("theory.html")
+
 @app.route('/test1')
 def test1():
-    connect = sqlite3.connect("database.db")
-    cursor = connect.cursor();
-    cursor.execute("select * from questions");
-    questions = cursor.fetchall()
-    connect.close();
+    questions = getDataFromDatabase() 
     return render_template("test.html", questions=questions) 
 
 @app.route('/test2')
