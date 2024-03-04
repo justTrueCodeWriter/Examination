66a96dcce205ca92898d00ba75897c8c00d1b6c7
justTrueCodeWriter <dmjustcoder8@gmail.com>
2024-03-04 19:56:16 +0300

    refactor && add access check

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ app = Flask(__name__)
 
 test_result = 0
 questions_count = 0
+access_level = 0
 
 def checkTestResult(answers, isTestFinished: bool) -> bool:
     global test_result
     test_result = 0
-    answer_id = 1
-    for answer in answers:
+    for answer_id in range(1, len(answers)):
         if (request.args.get(f"choice_box_element{answer_id}")):
             isTestFinished = True
         if (request.args.get(f"choice_box_element{answer_id}")=='1'):
             test_result+=1
-        answer_id += 1
     return isTestFinished
 
 @app.route('/')
@@ -28,35 +27,53 @@ def theory():
 
 @app.route('/test1')
 def test1():
+    test_level = 1
+
     isTestFinished = False
-    questions, answers = getDataFromDatabase(1) 
+    questions, answers = getDataFromDatabase(test_level) 
     global questios_count
+    global access_level
     max_score = getAmountOfCorrectAnswers(answers) 
     isTestFinished = checkTestResult(answers, isTestFinished)
+    if (isTestFinished):
+        access_level+=1
 
     return render_template("test.html", questions=questions, answers=answers, isTestFinished=isTestFinished, test_result=test_result, max_score=max_score) 
 
 @app.route('/test2')
 def test2():
+    test_level = 2
+
     isTestFinished = False
-    questions, answers = getDataFromDatabase(2) 
+    questions, answers = getDataFromDatabase(test_level) 
     global questios_count
+    global access_level
     max_score = getAmountOfCorrectAnswers(answers) 
     isTestFinished = checkTestResult(answers, isTestFinished)
+    if (isTestFinished):
+        access_level+=1
 
-    return render_template("test.html", questions=questions, answers=answers, isTestFinished=isTestFinished, test_result=test_result, max_score=max_score) 
+    if (access_level >= test_level-1):
+        return render_template("test.html", questions=questions, answers=answers, isTestFinished=isTestFinished, test_result=test_result, max_score=max_score) 
+    else:
+        message = "У вас нет доступа к этому тесту. Завершите предыдущий тест."
+        return render_template("message_page.html", message=message) 
 
 @app.route('/test3')
 def test3():
+    test_level = 3
+
     isTestFinished = False
-    questions, answers = getDataFromDatabase(3) 
+    questions, answers = getDataFromDatabase(test_level) 
     global questios_count
+    global access_level
     max_score = getAmountOfCorrectAnswers(answers) 
     isTestFinished = checkTestResult(answers, isTestFinished)
+    if (isTestFinished):
+        access_level+=1
 
-    return render_template("test.html", questions=questions, answers=answers, isTestFinished=isTestFinished, test_result=test_result, max_score=max_score) 
-
-@app.route('/examination')
-def examination():
-    questions, answers = getDataFromDatabase(4) 
-    return render_template("test.html", questions=questions, answers=answers) 
+    if (access_level >= test_level-1):
+        return render_template("test.html", questions=questions, answers=answers, isTestFinished=isTestFinished, test_result=test_result, max_score=max_score) 
+    else:
+        message = "У вас нет доступа к этому тесту. Завершите предыдущий тест."
+        return render_template("message_page.html", message=message) 
